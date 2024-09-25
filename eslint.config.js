199d0015d65c4b4096b6d37c9c1df3b2f920543f
lucasminatelli <lucas.minatelli@gmail.com>
2024-09-25 10:16:12 -0300

    feats orbit configs

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+  ignorePatterns: [
+    "webpack/**",
+    ".eslintrc.js",
+    "config/jest/transformers/**",
+    "jest.config.js",
+  ],
+};
