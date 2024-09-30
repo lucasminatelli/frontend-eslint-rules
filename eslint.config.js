b9a2c289dd57773da48ce01fb548dd424eef88bc
lucasminatelli <lucas.minatelli@gmail.com>
2024-09-30 10:48:55 -0300

    indicates some rule conventions

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,23 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    // "@typescript-eslint/no-unnecessary-condition": "error",
+    // removes unnecessary conditional checks and optional chaining. Prevent against empty objects and arrays evaluate to true. Handles pending promises.
+    // ---
+    // "no-shadow": "off",
+    // "@typescript-eslint/no-shadow": "error"
+    // Note: you must disable the base rule as it can report incorrect errors. Prevents to share variables names across different scopes.
+    // ---
+    //"@typescript-eslint/naming-convention": "error"
+    // Defines naming convention rules inside the project. https://typescript-eslint.io/rules/naming-convention/
+    // ---
+    // "require-await": "off",
+    // "@typescript-eslint/require-await": "error"
+    // Note: you must disable the base rule as it can report incorrect errors. Disallow async functions which have no await expression
+    // ---
+    // Folders and files naming: https://github.com/christopherbradleybanks/eslint-plugin-folders https://github.com/dolsem/eslint-plugin-filename-rules
+  },
   ignorePatterns: [
     "webpack/**",
     ".eslintrc.js",
